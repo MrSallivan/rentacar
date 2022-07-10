a84ccf9e3c5fc04f7db10ec706b7ee2d85bc1b89
MrSallivan <nik.sallivan@gmail.com>
2022-07-10 23:19:57 +1000

    owl1

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,12 +68,8 @@
 
 		// Partner Carousel
 		$(".owl-carousel-cars").owlCarousel({
-			animateOut: 'slideOutDown',
-			animateIn: 'flipInX',
-			items: 1,
-			margin: 30,
-			stagePadding: 30,
-			smartSpeed: 450
+			items: 2
+			
 		});
 
 

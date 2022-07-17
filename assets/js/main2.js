883811677f4ccca1b344ef569f2fd1340989a86b
MrSallivan <nik.sallivan@gmail.com>
2022-07-17 23:19:37 +1000

    main and js

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -1,9 +1,21 @@
-let orderBtns = document.querySelectorAll('.car-card__order')
+let orderBtns = document.querySelectorAll('a.car-card__order')
+console.log(orderBtns)
 
-for (let orderBtn of orderBtns) {
-	orderBtn.addEventListener('click', (e) => {
+orderBtns.forEach(item => {
+	item.addEventListener('click', (e) => {
 		let parentNode = e.target.parentNode.parentNode
 		console.log(parentNode)
-
+		let cartPropertys = parentNode.nextSibling.previousSibling.children[2]
+		console.log(cartPropertys)
+		let carBrand = cartPropertys.children[0].children[0].innerHTML
+		console.log(carBrand)
+		let carYear = cartPropertys.children[1].children[0].innerHTML
+		console.log(carYear)
+		let carEngine = cartPropertys.children[2].children[0].innerHTML
+		console.log(carEngine)
+		let carDrive = cartPropertys.children[3].children[0].innerHTML
+		console.log(carDrive)
+		let carTransmis = cartPropertys.children[4].children[0].innerHTML
+		console.log(carTransmis)
 	})
-}
+})
